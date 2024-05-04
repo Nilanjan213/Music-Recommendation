65c45c472cc262e2acd29ded54ec2463cda84a48
Nilanjan Saha <139051041+Nilanjan213@users.noreply.github.com>
2024-05-05 00:34:14 +0530

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -3,26 +3,19 @@ import numpy as np
 import cv2 
  
 cap = cv2.VideoCapture(0)
-
 name = input("Enter the name of the data : ")
-
 holistic = mp.solutions.holistic
 hands = mp.solutions.hands
 holis = holistic.Holistic()
 drawing = mp.solutions.drawing_utils
-
 X = []
 data_size = 0
 
 while True:
 	lst = []
-
 	_, frm = cap.read()
-
 	frm = cv2.flip(frm, 1)
-
 	res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
-
 
 	if res.face_landmarks:
 		for i in res.face_landmarks.landmark:
@@ -45,11 +38,8 @@ while True:
 			for i in range(42):
 				lst.append(0.0)
 
-
 		X.append(lst)
 		data_size = data_size+1
-
-
 
 	drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_CONTOURS)
 	drawing.draw_landmarks(frm, res.left_hand_landmarks, hands.HAND_CONNECTIONS)
